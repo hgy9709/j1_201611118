f312fb6ce5a684f85378c02ae025617ad5459508
hgy9709 <hgy9709@naver.com>
2016-10-10 12:13:17 +0900

    Add files via upload

---
+++ dice.java
@@ -0,0 +1,10 @@
+public class Dice{ 
+    private int number = 0; 
+	public void roll() { 
+		number = (int) (Math.random()*6)+1; 
+		System.out.println("Turns on " +number);
+	}
+	public int getNum() { 
+		return this.number; 
+	} 
+} 
