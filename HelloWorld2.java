9daf316302741b24412421714f104dda59b170b3
hgy9709 <hgy9709@naver.com>
2016-11-06 17:55:36 +0900

    HelloWorld2 package

---
+++ HelloWorld2.java
@@ -0,0 +1,13 @@
+package com.sd.hello;
+
+public class HelloWorld2 {
+  public void hello() {
+    System.out.println("Hello World!");
+  }
+  public void hello(String name) {
+    System.out.printf("Hello World %s!", name);
+  }
+  public static void main(String[] args) {
+    System.out.println("Hello World!!!");
+  }
+}
