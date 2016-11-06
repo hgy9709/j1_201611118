eb40859ec6b537e2a2b190f0f167dcf9d6c4bab0
hgy9709 <hgy9709@naver.com>
2016-11-06 18:29:20 +0900

    TurtleDemoMain

---
+++ TurtleDemoMain.java
@@ -0,0 +1,15 @@
+package com.sd.turtle;
+import ch.aplu.turtle.*;
+
+public class TurtleDemoMain {
+	Turtle t1 = new Turtle();
+	TurtleDemonMain() {
+		for(int i=0;i<4;i++) {
+			t1.fd(80);
+			t1.left(90);
+			}
+		}
+	public static void main(String[] args){
+		new TurtleDemoMian();
+	}
+}
