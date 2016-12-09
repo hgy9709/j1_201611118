03f63d058e371db1d89e5ed80c322f5ec6e637ce
hgy9709 <hgy9709@naver.com>
2016-12-10 02:09:24 +0900

    Add files via upload

---
+++ CalcAreaGUIMain.java
@@ -0,0 +1,33 @@
+package com.sd.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+// import javax.swing.JFrame;
+// import javax.swing.JButton;
+// import java.awt.Container;
+
+
+public class CalcAreaGUIMain {
+	public static void main(String[] args) {
+		JFrame f= new JFrame();
+		JPanel panel = new JPanel(new GridLayout(3,2));
+		f.getContentPane().add(panel);
+		JLabel label1 = new JLabel("Radius");
+		JLabel label2 = new JLabel("Area");
+		JTextField radiusTf1= new JTextField(10);
+		JTextField areaTf2= new JTextField(10);
+		JButton b1 = new JButton("Compute");
+		JButton b2 = new JButton("Cancel");
+		panel.add(label1);
+		panel.add(radiusTf1);
+		panel.add(label2);
+		panel.add(areaTf2);
+		panel.add(b1);
+		panel.add(b2);
+		f.pack();
+		f.setVisible(true);
+
+	}
+
+}
